1392c1134bef50beb39546951e0698926ed7eb9b
lemon-exe <hotmail25132@gmail.com>
2023-08-12 16:25:24 -0400

    try again >:(

--- popup.js
+++ popup.js
@@ -1,41 +1,54 @@
 const FULL_DASH_ARRAY = 283;
-const RESET_DASH_ARRAY = `-57 ${FULL_DASH_ARRAY}`;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
 
-//All buttons
-let startBtn = document.querySelector(".start");
-let stopBtn = document.querySelector(".stop");
-let resetBtn = document.querySelector(".reset");
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
 
-//DOM elements
-let timer = document.querySelector("#base-timer-path-remaining");
-let timeLabel = document.getElementById("base-timer-label");
-
-//Time related vars
-const TIME_LIMIT = 30; //in seconds
-let timePassed = -1;
+const TIME_LIMIT = 20;
+let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
 
-function reset() {
-  clearInterval(timerInterval);
-  resetVars();
-  startBtn.innerHTML = "Start";
-  timer.setAttribute("stroke-dasharray", RESET_DASH_ARRAY);
-}
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
 
-function start(withReset = false) {
-  setDisabled(startBtn);
-  removeDisabled(stopBtn);
-  if (withReset) {
-    resetVars();
-  }
-  startTimer();
-}
+startTimer();
 
-function stop() {
-  setDisabled(stopBtn);
-  removeDisabled(startBtn);
-  startBtn.innerHTML = "Continue";
+function onTimesUp() {
   clearInterval(timerInterval);
 }
 
@@ -43,50 +56,16 @@ function startTimer() {
   timerInterval = setInterval(() => {
     timePassed = timePassed += 1;
     timeLeft = TIME_LIMIT - timePassed;
-    timeLabel.innerHTML = formatTime(timeLeft);
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
     setCircleDasharray();
+    setRemainingPathColor(timeLeft);
 
     if (timeLeft === 0) {
-      timeIsUp();
+      onTimesUp();
     }
   }, 1000);
-}
-
-window.addEventListener("load", () => {
-  timeLabel.innerHTML = formatTime(TIME_LIMIT);
-  setDisabled(stopBtn);
-});
-
-//---------------------------------------------
-//HELPER METHODS
-//---------------------------------------------
-function setDisabled(button) {
-  button.setAttribute("disabled", "disabled");
-}
-
-function removeDisabled(button) {
-  button.removeAttribute("disabled");
-}
-function timeIsUp() {
-  setDisabled(startBtn);
-  removeDisabled(stopBtn);
-  clearInterval(timerInterval);
-  let confirmReset = confirm("Time is UP! Wanna restart?");
-  if (confirmReset) {
-    reset();
-    startTimer();
-  } else {
-    reset();
-  }
-}
-
-function resetVars() {
-  removeDisabled(startBtn);
-  setDisabled(stopBtn);
-  timePassed = -1;
-  timeLeft = TIME_LIMIT;
-  console.log(timePassed, timeLeft);
-  timeLabel.innerHTML = formatTime(TIME_LIMIT);
 }
 
 function formatTime(time) {
@@ -100,6 +79,25 @@ function formatTime(time) {
   return `${minutes}:${seconds}`;
 }
 
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
 function calculateTimeFraction() {
   const rawTimeFraction = timeLeft / TIME_LIMIT;
   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
@@ -109,6 +107,7 @@ function setCircleDasharray() {
   const circleDasharray = `${(
     calculateTimeFraction() * FULL_DASH_ARRAY
   ).toFixed(0)} 283`;
-  console.log("setCircleDashArray: ", circleDasharray);
-  timer.setAttribute("stroke-dasharray", circleDasharray);
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
 }
