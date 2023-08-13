1cc62687bc51bf8ea490a548af12673ee79b7b33
lemon-exe <hotmail25132@gmail.com>
2023-08-13 12:19:33 -0400

    adding audio tracks in playlist - jaden

--- audio.js
+++ audio.js
@@ -2,10 +2,41 @@ document.addEventListener('DOMContentLoaded', function() {
     const playImage = document.getElementById('playImage');
     const pauseImage = document.getElementById('pauseImage');
     const audioPlayer = document.getElementById('audioPlayer');
-    audioPlayer.volume = 0.1;
+    const currentFileDisplay = document.getElementById("currentFile");
+    const playlist = [
+        "Believe in Miracle.mp3",
+        "Close Study.mp3",
+        "Coding Night.mp3",
+        "Inspiring Atmosphere.mp3",
+        "Just Relax.mp3",
+        "Nature-9.mp3",
+        "Lofi Girl Dreams.mp3",
+        "Love Romantic.mp3",
+        "Shooting Stars.mp3",
+        "Untitled Track.mp3",
+        "Nature.mp3"
+    ]
 
+    let i = 0;
+    let start = false;
+    function nextSong(play = true){
+        audioPlayer.src = "Assets/Tracks/" + playlist[i];
+        if(play) audioPlayer.play();
+        currentFileDisplay.textContent = `Playing: ${playlist[i]}`;
+        if(play) {
+            i = (i+1) % playlist.length;
+            console.log("nextsong");
+        }
+    }
+
+
+    audioPlayer.volume = 0.2;
+    audioPlayer.addEventListener('ended', nextSong(true));
     playImage.addEventListener('click', function() {
-    
+        if(!start){
+            nextSong();
+            start = true;
+        }
         audioPlayer.play();
         playImage.style.display = 'none';   // Hide play image
         pauseImage.style.display = 'block'; // Show pause image
@@ -18,4 +49,7 @@ document.addEventListener('DOMContentLoaded', function() {
         pauseImage.style.display = 'none';  // Hide pause image
         
     });
+
+
+    nextSong(false);
 });
