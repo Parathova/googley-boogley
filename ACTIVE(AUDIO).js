3920e5e9708c47c62b1072b6dc9b734bc9c704b7
watson <joycew0343@gmail.com>
2023-08-14 10:37:52 -0400

    new tracks

--- ACTIVE(AUDIO).js
+++ ACTIVE(AUDIO).js
@@ -19,7 +19,15 @@ document.addEventListener('DOMContentLoaded', function() {
         "Love Romantic.mp3",
         "Shooting Stars.mp3",
         "Untitled Track.mp3",
-        "Nature.mp3"
+        "Nature.mp3",
+        "Lofi Study.mp3",
+        "Lofi Chill.mp3",
+        "Please Calm My Mind.mp3",
+        "Empty Hour.mp3",
+        "Valley of Hope.mp3",
+        "Sleepy Cat.mp3",
+        "Motivated To Create.mp3",
+        "Wish You Were Here.mp3",
     ]
 
     let i = 0;
